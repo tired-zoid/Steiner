1aad2bb971550a480f12f52e163ebada5d5c7484
tired-zoid <rahimzoid@gmail.com>
2022-11-18 16:54:45 +0300

    load

---
+++ Steiner/Form1.Designer.cs
@@ -0,0 +1,264 @@
+﻿namespace Steiner
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label2 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.numericUpDown1 = new System.Windows.Forms.NumericUpDown();
+            this.numericUpDown2 = new System.Windows.Forms.NumericUpDown();
+            this.button1 = new System.Windows.Forms.Button();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.label9 = new System.Windows.Forms.Label();
+            this.dataGridView3 = new System.Windows.Forms.DataGridView();
+            this.label8 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.dataGridView2 = new System.Windows.Forms.DataGridView();
+            this.label3 = new System.Windows.Forms.Label();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.label7 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown2)).BeginInit();
+            this.groupBox1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView3)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(6, 35);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(249, 13);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "Размеры квадратной координатной плоскости:";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(6, 56);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(89, 13);
+            this.label4.TabIndex = 5;
+            this.label4.Text = "Диапазон X  до:";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(254, 35);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(127, 13);
+            this.label6.TabIndex = 7;
+            this.label6.Text = "Количество элементов:";
+            // 
+            // numericUpDown1
+            // 
+            this.numericUpDown1.Location = new System.Drawing.Point(101, 54);
+            this.numericUpDown1.Name = "numericUpDown1";
+            this.numericUpDown1.Size = new System.Drawing.Size(62, 20);
+            this.numericUpDown1.TabIndex = 10;
+            // 
+            // numericUpDown2
+            // 
+            this.numericUpDown2.Location = new System.Drawing.Point(256, 52);
+            this.numericUpDown2.Name = "numericUpDown2";
+            this.numericUpDown2.Size = new System.Drawing.Size(109, 20);
+            this.numericUpDown2.TabIndex = 11;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(6, 78);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(358, 35);
+            this.button1.TabIndex = 14;
+            this.button1.Text = "Разместить элементы на координатной плоскости  и выполнить трассировку";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click_1);
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.label9);
+            this.groupBox1.Controls.Add(this.dataGridView3);
+            this.groupBox1.Controls.Add(this.label8);
+            this.groupBox1.Controls.Add(this.label5);
+            this.groupBox1.Controls.Add(this.dataGridView2);
+            this.groupBox1.Controls.Add(this.label3);
+            this.groupBox1.Controls.Add(this.textBox2);
+            this.groupBox1.Controls.Add(this.textBox1);
+            this.groupBox1.Controls.Add(this.dataGridView1);
+            this.groupBox1.Controls.Add(this.label7);
+            this.groupBox1.Controls.Add(this.button1);
+            this.groupBox1.Controls.Add(this.label2);
+            this.groupBox1.Controls.Add(this.label4);
+            this.groupBox1.Controls.Add(this.numericUpDown2);
+            this.groupBox1.Controls.Add(this.label6);
+            this.groupBox1.Controls.Add(this.numericUpDown1);
+            this.groupBox1.Location = new System.Drawing.Point(12, 12);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(931, 369);
+            this.groupBox1.TabIndex = 15;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Ввод исходных данных";
+            // 
+            // label9
+            // 
+            this.label9.AutoSize = true;
+            this.label9.Location = new System.Drawing.Point(454, 343);
+            this.label9.Name = "label9";
+            this.label9.Size = new System.Drawing.Size(238, 13);
+            this.label9.TabIndex = 30;
+            this.label9.Text = "Общее количество точек в дереве Штейнера ";
+            // 
+            // dataGridView3
+            // 
+            this.dataGridView3.AutoSizeColumnsMode = System.Windows.Forms.DataGridViewAutoSizeColumnsMode.Fill;
+            this.dataGridView3.AutoSizeRowsMode = System.Windows.Forms.DataGridViewAutoSizeRowsMode.AllCells;
+            this.dataGridView3.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView3.Location = new System.Drawing.Point(399, 187);
+            this.dataGridView3.Name = "dataGridView3";
+            this.dataGridView3.RowHeadersWidth = 51;
+            this.dataGridView3.Size = new System.Drawing.Size(532, 123);
+            this.dataGridView3.TabIndex = 24;
+            // 
+            // label8
+            // 
+            this.label8.AutoSize = true;
+            this.label8.Location = new System.Drawing.Point(396, 171);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(100, 13);
+            this.label8.TabIndex = 23;
+            this.label8.Text = "Координаты точек";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(0, 181);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(153, 13);
+            this.label5.TabIndex = 22;
+            this.label5.Text = "Список ребер дерева Прима";
+            // 
+            // dataGridView2
+            // 
+            this.dataGridView2.AutoSizeColumnsMode = System.Windows.Forms.DataGridViewAutoSizeColumnsMode.Fill;
+            this.dataGridView2.AutoSizeRowsMode = System.Windows.Forms.DataGridViewAutoSizeRowsMode.AllCells;
+            this.dataGridView2.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView2.Location = new System.Drawing.Point(0, 207);
+            this.dataGridView2.Name = "dataGridView2";
+            this.dataGridView2.RowHeadersWidth = 51;
+            this.dataGridView2.Size = new System.Drawing.Size(203, 149);
+            this.dataGridView2.TabIndex = 21;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(0, 135);
+            this.label3.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(190, 13);
+            this.label3.TabIndex = 20;
+            this.label3.Text = "Минимальная сумма дерева Прима";
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(2, 152);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(163, 20);
+            this.textBox2.TabIndex = 19;
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(698, 340);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(76, 20);
+            this.textBox1.TabIndex = 18;
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.AutoSizeColumnsMode = System.Windows.Forms.DataGridViewAutoSizeColumnsMode.Fill;
+            this.dataGridView1.AutoSizeRowsMode = System.Windows.Forms.DataGridViewAutoSizeRowsMode.AllCells;
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Location = new System.Drawing.Point(398, 35);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.RowHeadersWidth = 51;
+            this.dataGridView1.Size = new System.Drawing.Size(532, 113);
+            this.dataGridView1.TabIndex = 0;
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Location = new System.Drawing.Point(395, 15);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(116, 13);
+            this.label7.TabIndex = 2;
+            this.label7.Text = "Матрица расстояний ";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(970, 412);
+            this.Controls.Add(this.groupBox1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown2)).EndInit();
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView3)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.NumericUpDown numericUpDown1;
+        private System.Windows.Forms.NumericUpDown numericUpDown2;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.DataGridView dataGridView2;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label8;
+        private System.Windows.Forms.DataGridView dataGridView3;
+        private System.Windows.Forms.Label label9;
+    }
+}
+
---
+++ Steiner/Form1.cs
@@ -0,0 +1,452 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Steiner
+{
+    public partial class Form1 : Form
+    {
+        int[,] usedE;
+        Random rnd = new Random();
+        int dimX;
+        public int topQ = 0;
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        public void button1_Click_1(object sender, EventArgs e)
+        {
+             topQ = Convert.ToInt32(numericUpDown2.Value);
+            dimX = Convert.ToInt32(numericUpDown1.Value);
+            int[,] matRas = new int[topQ, topQ];
+            int[,] elCoord = new int[2, topQ];
+            Placement(ref matRas, elCoord,  topQ, dimX);
+            dataGridView1.Rows.Clear();
+            dataGridView1.Columns.Clear();
+            dataGridView2.Rows.Clear();
+            dataGridView2.Columns.Clear();
+            dataGridView3.Rows.Clear();
+            dataGridView3.Columns.Clear();
+
+            for (int i = 0; i < matRas.GetLength(0); i++)
+            {
+                dataGridView1.Columns.Add("Column1", "X");
+                dataGridView1.Rows.Add();
+            }
+            for (int i = 0; i < matRas.GetLength(0); i++)
+            {
+                for (int j = 0; j < matRas.GetLength(0); j++)
+                {
+                    dataGridView1.Rows[i].Cells[j].Value = matRas[i, j];
+                }
+            }
+
+
+            for (int i = 0; i < elCoord.GetLength(1); i++)
+            {
+                dataGridView3.Columns.Add("Column1", "T");
+                dataGridView3.Rows.Add();
+            }
+            for (int i = 0; i < elCoord.GetLength(0); i++)
+            {
+                for (int j = 0; j < elCoord.GetLength(1); j++)
+                {
+                    dataGridView3.Rows[i].Cells[j].Value = elCoord[i, j];
+                }
+            }
+            int length = 0;
+            int[,] usedE = new int[2, topQ];
+            int no_edge;
+            bool[] selected = new bool[topQ];
+            no_edge = 0;
+            selected[0] = true;
+            int x;
+            int y;
+             // Prima 
+            while (no_edge < topQ - 1)
+            {
+                int min = 9999;
+                x = 0;
+                y = 0;
+
+                for (int i = 0; i < topQ; i++)
+                {
+                    if (selected[i])
+                    {
+                        for (int j = 0; j < topQ; j++)
+                        {
+                            if (!selected[j])
+                            {
+                                if (matRas[i, j] < min)
+                                {
+                                    min = matRas[i, j];
+                                    x = i;
+                                    y = j;
+                                }
+
+                            }
+                        }
+                    }
+                }
+                usedE[0, no_edge] = x;
+                usedE[1, no_edge] = y;
+                length += matRas[x, y];
+                selected[y] = true;
+                no_edge++;
+            }
+
+
+            int[,] firstEdge = new int[2, dimX*dimX];
+
+            for (int i = 0; i < firstEdge.GetLength(0); i++)
+            {
+                for (int j = 0; j < firstEdge.GetLength(1); j++)
+                {
+                    firstEdge[i, j] = 0;
+                }
+            }
+            int[,] secondEdge = new int[2, dimX*dimX];
+
+            for (int i = 0; i < firstEdge.GetLength(0); i++)
+            {
+                for (int j = 0; j < firstEdge.GetLength(1); j++)
+                {
+                    firstEdge[i, j] = 0;                }
+            }
+
+            int[,] ST = new int[2, dimX * dimX];
+
+            for (int i = 0; i < ST.GetLength(0); i++)
+            {
+                for (int j = 0; j < ST.GetLength(1); j++)
+                {
+                    ST[i, j] = 0;
+                }
+            }
+
+            int g = 0;
+            int h = 0;
+            int x1 = 0;
+            int y1 = 0;
+            int x2 = 0;
+            int y2 = 0;
+            int m = 0;
+            int n = 0;
+
+
+            while (m != topQ - 1)
+            {
+                g = usedE[0, m];
+                h = usedE[1, m];
+                x1 = elCoord[0, g];
+                y1 = elCoord[1, g];
+                x2 = elCoord[0, h];
+                y2 = elCoord[1, h];
+                int p = 0;
+                int q = 0;
+
+                x1 = elCoord[0, g];
+                y1 = elCoord[1, g];
+                x2 = elCoord[0, h];
+                y2 = elCoord[1, h];
+
+                // первая конфигурация 
+                while (x1 != x2 && y1 != y2) 
+                {
+
+                    while (x1 != x2)
+                    {
+                        if (x1 <= x2)
+                        {
+                            firstEdge[0, p] = x1;
+                            firstEdge[1, p] = y1;
+                            x1++;
+                            p++;
+                        }
+                        else
+                        {
+                            firstEdge[0, p] = x1;
+                            firstEdge[1, p] = y1;
+                            x1--;
+                            p++;
+                        }
+                    }
+
+                    while (y2 != y1)
+                    {
+
+                        if (y2 <= y1)
+                        {
+                            firstEdge[0, p] = x2;
+                            firstEdge[1, p] = y2;
+                            y2++;
+                            p++;
+                        }
+                        else
+                        {
+                            firstEdge[0, p] = x2;
+                            firstEdge[1, p] = y2;
+                            y2--;
+                            p++;
+                        }
+                    }
+
+                }
+
+                x1 = elCoord[0, g];
+                y1 = elCoord[1, g];
+                x2 = elCoord[0, h];
+                y2 = elCoord[1, h];
+
+                // вторая конфигурация 
+                while (x1 != x2 && y1 != y2) 
+                {
+
+                    while (y1 != y2)
+                    {
+                        if (y1 <= y2)
+                        {
+                            secondEdge[0, q] = x1;
+                            secondEdge[1, q] = y1;
+                            y1++;
+                            q++;
+                        }
+                        else
+                        {
+                            secondEdge[0, q] = x1;
+                            secondEdge[1, q] = y1;
+                            y1--;
+                            q++;
+                        }
+                    }
+
+                    while (x2 != x1)
+                    {
+
+                        if (x2 <= x1)
+                        {
+                            secondEdge[0, q] = x2;
+                            secondEdge[1, q] = y2;
+                            x2++;
+                            q++;
+                        }
+                        else
+                        {
+                            secondEdge[0, q] = x2;
+                            secondEdge[1, q] = y2;
+                            x2--;
+                            q++;
+                        }
+                    }
+
+                }
+
+                ST[0, 0] = elCoord[0,0]; // добавляем координаты первой точки в дерево Штейнера
+                ST[1, 0] = elCoord[1,0];       
+
+                int[,] points1 = new int[2, dimX * dimX]; // сравниваем первую конфигурацию 
+                int k = 0;
+                for (int i = 0; i < firstEdge.GetLength(0); i++)
+                {
+                    for (int j = 0; j < firstEdge.GetLength(1); j++)
+                    {
+                        if (ST[i, j] > 0)
+                        {
+                            if (firstEdge[i, j] == ST[i, j])
+                            {
+                                points1[0, k] = i;
+                                points1[1, k] = j;
+                                k++;
+                            }
+
+                        }
+
+                    }
+                }
+
+                int[,] points2 = new int[2, dimX * dimX]; // сравниваем вторую конфигурацию с деревом 
+                int z = 0;
+                for (int i = 0; i < secondEdge.GetLength(0); i++)
+                {
+                    for (int j = 0; j < secondEdge.GetLength(1); j++)
+                    {
+                        if (ST[i, j] > 0)
+                        {
+                            if (secondEdge[i, j] == ST[i, j])
+                            {
+                                points2[0, z] = i;
+                                points2[1, z] = j;
+                                z++;
+                            }
+                        }
+
+                    }
+                }
+
+                int numP1 = 0; // подсчет ненулевых элементов совпадение первого ребра с деревом 
+                for (int i = 0; i < points1.GetLength(0); i++) 
+                {
+                    for (int j = 0; j < points1.GetLength(1); j++)
+                    {
+                        if ((points1[0, j]  != 0) && (points1[1, j] !=0))
+                        {
+                            numP1++;
+                        }
+
+                    }
+                }
+
+                int numP2 = 0; // подсчет ненулевых элементов совпадений второго ребра с деревом 
+                for (int i = 0; i < points2.GetLength(0); i++) 
+                {
+                    for (int j = 0; j < points2.GetLength(1); j++)
+                    {
+                        if ((points2[0, j] != 0) && (points2[1, j] != 0))
+                        {
+                            numP2++;
+                        }
+
+                    }
+                }
+
+
+                int l = 0;   // добавление первой конфигурации в дерево
+                if (numP1 >= numP2)
+                {
+                    while ((firstEdge[0,l] > 0) || (firstEdge[1, l] > 0))  
+                    {
+                        for (int i = 0; i < ST.GetLength(1); i++)
+                        {
+                            if (ST[0, i] == 0 && ST[1, i] == 0)
+                            {
+                                ST[0, i] = firstEdge[0, l];
+                                ST[1, i] = firstEdge[1, l];
+                                l++;
+                            }
+                        }
+                    }
+                }
+                            // добавление второй конфигурации в дерево
+                else
+                {
+                    while ((secondEdge[0, l] > 0) || (secondEdge[1, l] > 0))
+                    {
+
+                            for (int i = 0; i < ST.GetLength(1); i++)
+                            {
+                                if (ST[0, i] == 0 && ST[1, i] == 0)
+                                {
+                                    ST[0, i] = secondEdge[0, l];
+                                    ST[1, i] = secondEdge[1, l];
+                                    l++;
+                                }
+                            }
+                        
+                    }
+                }
+                    
+            m++;
+            }
+
+
+
+            int[,] lastST = new int[2, dimX * dimX];
+            lastST[0, 0] = ST[0, 0];
+            int b = 0;
+            for (int i = 0; i < ST.GetLength(1); i++)
+            {
+
+                if (lastST[0,i] == ST[0,b])
+                {
+                    if (lastST[1,i] == ST[1,b])
+                    {
+                        b++;
+                    }
+                    else
+                    {
+                        lastST[0, i] = ST[0, b];
+                        lastST[1, i] = ST[1, b];
+                        b++;
+                    }
+                }
+                else
+                {
+                    lastST[0, i] = ST[0, b];
+                    lastST[1, i] = ST[1, b];
+                    b++;
+                }
+
+            }
+
+                int pointsST = 0;
+
+                for (int j = 0; j < lastST.GetLength(1); j++)
+                {
+                    if ((lastST[0, j] != 0) && (lastST[1, j] != 0))
+                    {
+                        pointsST++;
+                    }
+
+                }
+
+
+            textBox1.Text = pointsST.ToString();
+            textBox2.Text = length.ToString();
+            dataGridView2.Columns.Add("Column1", "X");
+            dataGridView2.Columns.Add("Column2", "Y");
+            for (int i = 0; i < usedE.GetLength(1) - 1; i++)
+            {
+                dataGridView2.Rows.Add();
+            }
+
+            for (int i = 0; i < usedE.GetLength(1) - 1; i++)
+            {
+                for (int j = 0; j < usedE.GetLength(0); j++)
+                {
+                    dataGridView2.Rows[i].Cells[j].Value = usedE[j, i] + 1;
+                }
+            }
+        }
+
+        public (int[,], int [,]) Placement(ref int[,] matRas, int [,] elCoord, int topQ, int dimX)
+        {
+            for (int i = 0; i < elCoord.GetLength(0); i++)
+            {
+                for (int j = 0; j < elCoord.GetLength(1); j++)
+                {
+                    elCoord[i, j] = rnd.Next(dimX);
+                }
+            }
+
+            int a = 0;
+            int b = 0;
+
+            for (int x = 0; x < matRas.GetLength(0); x++)
+            {
+                for (int y = 0; y < matRas.GetLength(0); y++)
+                {
+                    // евклидово расстояние 
+                    int dX = elCoord[0, a] - elCoord[0, b];
+                    int dY = elCoord[1, a] - elCoord[1, b];
+                    double distance = Math.Sqrt(dX * dX + dY * dY);
+                    matRas[x, y] = Convert.ToInt32(distance);
+                    dX = 0;
+                    dY = 0;
+                    b++;
+                }
+                b = 0;
+                a++;
+            }
+            return (matRas, elCoord);
+        }
+    }
+}
+
